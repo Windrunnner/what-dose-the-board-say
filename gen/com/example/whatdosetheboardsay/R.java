93243adc9752219517aaa6e47a2a1ff189a8a576
Len <lenppqq@163.com>
2014-03-01 16:15:33 -0500

    delete unused

--- gen/com/example/whatdosetheboardsay/R.java
+++ gen/com/example/whatdosetheboardsay/R.java
@@ -24,35 +24,15 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_forgot_password=0x7f08000e;
-        public static final int action_settings=0x7f08000d;
-        public static final int button1=0x7f080008;
-        public static final int button2=0x7f08000a;
-        public static final int button3=0x7f08000c;
-        public static final int editText1=0x7f080009;
-        public static final int editText4=0x7f08000b;
-        public static final int email=0x7f080003;
-        public static final int login=0x7f080005;
-        public static final int login_form=0x7f080002;
-        public static final int login_status=0x7f080000;
-        public static final int login_status_message=0x7f080001;
-        public static final int password=0x7f080004;
-        public static final int sign_in_button=0x7f080006;
-        public static final int textView1=0x7f080007;
+        public static final int action_settings=0x7f080006;
+        public static final int button1=0x7f080001;
+        public static final int button2=0x7f080003;
+        public static final int button3=0x7f080005;
+        public static final int editText1=0x7f080002;
+        public static final int editText4=0x7f080004;
+        public static final int textView1=0x7f080000;
     }
     public static final class layout {
-<<<<<<< HEAD
-        public static final int activity_display_message=0x7f030000;
-        public static final int activity_join=0x7f030001;
-        public static final int activity_main=0x7f030002;
-        public static final int activity_start_up_interface=0x7f030003;
-    }
-    public static final class menu {
-        public static final int display_message=0x7f070000;
-        public static final int join=0x7f070001;
-        public static final int main=0x7f070002;
-        public static final int start_up_interface=0x7f070003;
-=======
         public static final int activity_create_interface=0x7f030000;
         public static final int activity_display_message=0x7f030001;
         public static final int activity_join_interface_activity=0x7f030002;
@@ -65,32 +45,27 @@ public final class R {
         public static final int join_interface_activity=0x7f070002;
         public static final int main=0x7f070003;
         public static final int start_up_interface=0x7f070004;
->>>>>>> 55c6764ab8afafebeda5dd4c70257decc9463ee2
     }
     public static final class string {
-        public static final int action_forgot_password=0x7f05000a;
+        public static final int action_forgot_password=0x7f05000c;
         public static final int action_settings=0x7f050001;
-        public static final int action_sign_in_register=0x7f050008;
-        public static final int action_sign_in_short=0x7f050009;
+        public static final int action_sign_in_register=0x7f05000a;
+        public static final int action_sign_in_short=0x7f05000b;
         public static final int app_name=0x7f050000;
-        public static final int error_field_required=0x7f05000f;
-        public static final int error_incorrect_password=0x7f05000e;
-        public static final int error_invalid_email=0x7f05000c;
-        public static final int error_invalid_password=0x7f05000d;
+        public static final int error_field_required=0x7f050011;
+        public static final int error_incorrect_password=0x7f050010;
+        public static final int error_invalid_email=0x7f05000e;
+        public static final int error_invalid_password=0x7f05000f;
         public static final int hello_world=0x7f050002;
-<<<<<<< HEAD
-        public static final int login_progress_signing_in=0x7f05000b;
+        public static final int login_progress_signing_in=0x7f05000d;
         /**  Strings related to login 
          */
-        public static final int prompt_email=0x7f050006;
-        public static final int prompt_password=0x7f050007;
-        public static final int title_activity_display_message=0x7f050003;
-        public static final int title_activity_join=0x7f050005;
-=======
+        public static final int prompt_email=0x7f050008;
+        public static final int prompt_password=0x7f050009;
         public static final int title_activity_create_interface=0x7f050006;
         public static final int title_activity_display_message=0x7f050003;
+        public static final int title_activity_join=0x7f050007;
         public static final int title_activity_join_interface_activity=0x7f050005;
->>>>>>> 55c6764ab8afafebeda5dd4c70257decc9463ee2
         public static final int title_activity_start_up_interface=0x7f050004;
     }
     public static final class style {
