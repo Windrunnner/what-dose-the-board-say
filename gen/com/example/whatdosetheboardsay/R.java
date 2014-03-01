55c6764ab8afafebeda5dd4c70257decc9463ee2
“Edwin <wongyikfei@gmail.com>
2014-03-01 15:45:49 -0500

    Added Join and Create Interface

--- gen/com/example/whatdosetheboardsay/R.java
+++ gen/com/example/whatdosetheboardsay/R.java
@@ -33,20 +33,26 @@ public final class R {
         public static final int textView1=0x7f080000;
     }
     public static final class layout {
-        public static final int activity_display_message=0x7f030000;
-        public static final int activity_main=0x7f030001;
-        public static final int activity_start_up_interface=0x7f030002;
+        public static final int activity_create_interface=0x7f030000;
+        public static final int activity_display_message=0x7f030001;
+        public static final int activity_join_interface_activity=0x7f030002;
+        public static final int activity_main=0x7f030003;
+        public static final int activity_start_up_interface=0x7f030004;
     }
     public static final class menu {
-        public static final int display_message=0x7f070000;
-        public static final int main=0x7f070001;
-        public static final int start_up_interface=0x7f070002;
+        public static final int create_interface=0x7f070000;
+        public static final int display_message=0x7f070001;
+        public static final int join_interface_activity=0x7f070002;
+        public static final int main=0x7f070003;
+        public static final int start_up_interface=0x7f070004;
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
         public static final int hello_world=0x7f050002;
+        public static final int title_activity_create_interface=0x7f050006;
         public static final int title_activity_display_message=0x7f050003;
+        public static final int title_activity_join_interface_activity=0x7f050005;
         public static final int title_activity_start_up_interface=0x7f050004;
     }
     public static final class style {
