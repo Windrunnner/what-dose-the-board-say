69a7ce1b2e1c561295147d18f3ecd2677e06c01c
zhang881 <zhang881@purdue.edu>
2014-03-01 18:29:39 -0500

    Create interface added

--- gen/com/example/whatdosetheboardsay/R.java
+++ gen/com/example/whatdosetheboardsay/R.java
@@ -24,16 +24,24 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080009;
-        public static final int avatar_image=0x7f080008;
-        public static final int button1=0x7f080001;
-        public static final int button2=0x7f080003;
-        public static final int button3=0x7f080005;
-        public static final int editText1=0x7f080002;
-        public static final int editText4=0x7f080004;
-        public static final int start_create_button=0x7f080007;
-        public static final int start_join_button=0x7f080006;
-        public static final int textView1=0x7f080000;
+        public static final int action_settings=0x7f080011;
+        public static final int avatar_image=0x7f080010;
+        public static final int backButton=0x7f080004;
+        public static final int button1=0x7f08000a;
+        public static final int button2=0x7f08000b;
+        public static final int button3=0x7f08000d;
+        public static final int createButton=0x7f080005;
+        public static final int createTxt=0x7f080000;
+        public static final int editText1=0x7f080006;
+        public static final int editText2=0x7f080007;
+        public static final int editText3=0x7f080008;
+        public static final int editText4=0x7f08000c;
+        public static final int inviteTxt=0x7f080003;
+        public static final int ipAddressTxt=0x7f080001;
+        public static final int setPasswordTxt=0x7f080002;
+        public static final int start_create_button=0x7f08000f;
+        public static final int start_join_button=0x7f08000e;
+        public static final int textView1=0x7f080009;
     }
     public static final class layout {
         public static final int activity_create_interface=0x7f030000;
