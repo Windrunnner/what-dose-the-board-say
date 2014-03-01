f1ec8ef37434960047117031acb458d89fbd9710
“Edwin <wongyikfei@gmail.com>
2014-03-01 16:30:35 -0500

    modified the start up interface

--- gen/com/example/whatdosetheboardsay/R.java
+++ gen/com/example/whatdosetheboardsay/R.java
@@ -24,7 +24,8 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080006;
+        public static final int action_settings=0x7f080007;
+        public static final int avatar_image=0x7f080006;
         public static final int button1=0x7f080001;
         public static final int button2=0x7f080003;
         public static final int button3=0x7f080005;
